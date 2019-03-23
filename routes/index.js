24c5c7c197320c1a28fc4b1b5ea67e29fcdb9807
Brett Cimbalik <bcimbali@gmail.com>
2019-03-23 12:54:41 -0500

    Add ability for user to save store reviews

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('./../controllers/storeController');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -58,11 +59,19 @@ router.post('/account/reset/:token',
 
 router.get('/map', storeController.mapPage);
 // Add the middleware (isLoggedIn) to see if user is logged in before we display hearted stores
-router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.get('/hearts', 
+  authController.isLoggedIn, 
+  catchErrors(storeController.getHearts)
+);
 
-/**
- * API
- **/
+router.post('/reviews/:id', 
+  authController.isLoggedIn, 
+  catchErrors(reviewController.addReview)
+);
+
+/*********************************************************************************
+ *                                    API
+ *********************************************************************************/
 
 router.get('/api/search/', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
